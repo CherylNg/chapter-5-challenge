90677a9e9c546f77583674f4916f3289847f778b
CherylNg <61680146+CherylNg@users.noreply.github.com>
2020-05-13 10:44:33 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+basic.showString("Mind your posture!")
+edubitTrafficLightBit.setLed(LedColor.Red, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+edubitTrafficLightBit.setLed(LedColor.Yellow, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+edubitTrafficLightBit.setLed(LedColor.Green, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+basic.forever(function () {
+    if (edubitIrBit.isIrSensorTriggered()) {
+        basic.showIcon(IconNames.Happy)
+        music.startMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Once)
+        edubitTrafficLightBit.setLed(LedColor.Green, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.On))
+        basic.pause(500)
+    } else {
+        edubitTrafficLightBit.setLed(LedColor.Green, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+        music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once)
+        basic.showIcon(IconNames.Angry)
+        edubitTrafficLightBit.toggleLed(LedColor.Red)
+        basic.pause(200)
+    }
+})
